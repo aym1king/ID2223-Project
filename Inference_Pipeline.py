1b7799fc6964baf2156be67032f7b555075af63e
aym1king <49128030+aym1king@users.noreply.github.com>
2024-01-12 04:21:11 +0100

    Update Inference_Pipeline.py

--- Inference_Pipeline.py
+++ Inference_Pipeline.py
@@ -15,7 +15,7 @@ two_days_ago = (today - timedelta(2)).strftime('%Y-%m-%d')
 today = today.strftime('%Y-%m-%d')
 
 feature_view = fs.get_feature_view(name="lag_demand_and_weather", version=1)
-batch_data = feature_view.get_batch_data(start_time=yesterday, end_time=today)
+batch_data = feature_view.get_batch_data(start_time=two_days_ago, end_time=yesterday)
 
 def add_date_features(df):
     new_df = df.copy()
