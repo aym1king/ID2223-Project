a6b63e65a93fccf8f1159e297a4d57604d1f000b
aym1king <49128030+aym1king@users.noreply.github.com>
2024-01-12 07:25:40 +0100

    Update Inference_Pipeline.py

--- Inference_Pipeline.py
+++ Inference_Pipeline.py
@@ -61,11 +61,14 @@ monitor_df['settlement_date'] = monitor_df['settlement_date'].astype("datetime64
 monitor_fg.insert(monitor_df, write_options={"wait_for_job" : False})
 
 history_df = monitor_fg.read()
+print("This is history_df", history_df)
 # Add our prediction to the history, as the history_df won't have it - 
 # the insertion was done asynchronously, so it will take ~1 min to land on App
 history_df = pd.concat([history_df, monitor_df])
 history_df.set_index('settlement_date')
 history_df.sort_index(inplace=True)
+
+print("This is sorted history_df;", history_df)
 
 history_plot = history_df.set_axis(['england_wales_demand'], axis=1)
 
