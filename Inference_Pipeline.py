72b4cee9cd1c3aa6107e615e1f71449a16dbc7b8
aym1king <49128030+aym1king@users.noreply.github.com>
2024-01-12 07:13:13 +0100

    Update Inference_Pipeline.py

--- Inference_Pipeline.py
+++ Inference_Pipeline.py
@@ -11,12 +11,10 @@ import datetime
 from datetime import date, timedelta
 today = datetime.datetime.now()
 yesterday = (today - timedelta(1)).strftime('%Y-%m-%d')
-yesterday_2 = (today - timedelta(2)).strftime('%Y-%m-%d')
-yesterday_3 = (today - timedelta(3)).strftime('%Y-%m-%d')
 today = today.strftime('%Y-%m-%d')
 
 feature_view = fs.get_feature_view(name="lag_demand_and_weather", version=1)
-batch_data = feature_view.get_batch_data()#start_time=yesterday_3, end_time=yesterday)
+batch_data = feature_view.get_batch_data(start_time=yesterday, end_time=today)
 print("this is batch:", batch_data)
 
 def add_date_features(df):
@@ -48,9 +46,9 @@ monitor_fg = fs.get_or_create_feature_group(name="demand_predictions",
                                             description="Electricity Demand Forecasting Monitoring"
                                             )
 
-demand = y_pred
+demand = [y_pred]
 print("this is y_pred", y_pred)
-dates = [pd.Timestamp(yesterday_3), pd.Timestamp(yesterday_2), pd.Timestamp(yesterday)]
+dates = [pd.Timestamp(yesterday)]
 print("this is dates", dates)
 
 data = {
