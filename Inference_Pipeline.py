27486aba1221501d430233e98bfb1c722d9a1bce
aym1king <49128030+aym1king@users.noreply.github.com>
2024-01-12 08:12:13 +0100

    Update Inference_Pipeline.py

--- Inference_Pipeline.py
+++ Inference_Pipeline.py
@@ -64,8 +64,9 @@ history_df = monitor_fg.read()
 # Add our prediction to the history, as the history_df won't have it - 
 # the insertion was done asynchronously, so it will take ~1 min to land on App
 history_df = pd.concat([history_df, monitor_df])
-history_df['settlement_date'] = history_df['settlement_date'].tz_convert(None).astype("datetime64[ns]")
 history_df.set_index('settlement_date')
+history_df = history_df.tz_localize(None)
+history_df.index = history_df.index.astype("datetime64[ns]")
 history_df.sort_index(inplace=True)
 
 print(type(history_df))
